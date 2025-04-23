8b83532173a4885d7d7b6ee2a45ed23d7e24272f
ruiyli <ruiyuli1024@gmail.com>
2025-04-23 12:34:25 +0000

    Deploying to gh-pages from @ ruiyli/ruiyli.github.io@3117fa688d336a92d4e941e6268c9b67e3ed8259 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -405,16 +405,16 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
+            },},{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-our-paper-on-the-time-series-forecasting-is-accepted-by-the-13th-acm-international-conference-on-future-energy-systems-acm-e-energy-22",
+          title: 'Our paper on the time series forecasting is accepted by the 13th ACM...',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
